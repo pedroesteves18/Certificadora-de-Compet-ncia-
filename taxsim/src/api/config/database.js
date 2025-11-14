d890a8dd74c71dbebfd288b5941e5d3057068bfd
pedro <pedrolol264@gmail.com>
2025-11-14 12:47:30 -0300

    ajuste sequelize

--- taxsim/src/api/config/database.js
+++ taxsim/src/api/config/database.js
@@ -9,7 +9,10 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: "postgres", 
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: process.env.DB_SSL === "true" ? { require: true, rejectUnauthorized: false } : false
+    },
     logging: false,
   }
 );
